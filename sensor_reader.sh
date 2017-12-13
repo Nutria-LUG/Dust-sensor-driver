6fe25d7b5e6cd4604d8e9b4a7b31d148589f98e3
glabita <g.labita@vesenda.com>
2017-12-13 19:13:52 +0100

    rmv: Removed unused echo log.

--- sensor_reader.sh
+++ sensor_reader.sh
@@ -6,20 +6,14 @@ stty -F /dev/ttyUSB0 9600 raw
 
 INPUT=$(od --endian=big -x -N10 < /dev/ttyUSB0|head -n 1|cut -f2-10 -d" ");
 
-#output
-echo $INPUT
-echo " "
 
 FIRST4BYTES=$(echo $INPUT|cut -b1-4);
-echo $FIRST4BYTES
 
 if [ "$FIRST4BYTES" = "aac0" ]; then
-  echo "check for correct intro characters: ok"
-  logger "check for correct intro characters: ok"
+    logger "check for correct intro characters: ok"
 else
-  echo "incorrect sequence, exiting"
-  logger "incorrect sequence, exiting"
-  exit;
+    logger "incorrect sequence, exiting"
+    exit;
 fi
 
 PPM25LOW=$(echo $INPUT|cut -f2 -d " "|cut -b1-2);
@@ -41,8 +35,5 @@ PPM10=$(echo "scale=1;((( $PPM10HIGHDEC * 256 ) + $PPM10LOWDEC ) / 10 ) "|bc -l 
 logger "Concentrazione PPM25: $PPM25"
 logger "Concentrazione PPM10: $PPM10"
 
-echo "Concentrazione PPM25: $PPM25"
-echo "ConcentrazionePPM10: $PPM10"
-
-echo $PPM25 > $WDIR/etc/ppm25.txt
-echo $PPM10 > $WDIR/etc/ppm10.txt
+echo "pm25 $PPM25"
+echo "pm10 $PPM10"
